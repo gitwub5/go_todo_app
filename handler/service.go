d65d9f2ad4ba5f503c75a09eee7797a285105b1e
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-11-21 20:30:14 +0900

    [Fix] go generate code

--- handler/service.go
+++ handler/service.go
@@ -8,7 +8,7 @@ import (
 
 // handler 패키지로부터 비즈니스 로직과 데이터베이스 처리를 제외시키기 위해 서비스 인터페이스를 정의한다.
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService RegisterUserService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
 }
